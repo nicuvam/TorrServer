9d9ece783e2b6a8fa60e0e89fb3c4d64f11d68dc
Nicu <nicuvam@gmail.com>
2026-09-01 21:49:22 +0300

    fix(qbitsync): keep raw torrent name as import title

--- server/qbitsync/meta.go
+++ server/qbitsync/meta.go
@@ -1,8 +1,6 @@
 package qbitsync
 
 import (
-	"strings"
-
 	"server/rutor"
 )
 
@@ -10,13 +8,5 @@ func resolveTitle(hashHex, rawName string) string {
 	if found := rutor.FindByHash(hashHex); found != nil && found.Title != "" {
 		return found.Title
 	}
-	return prettyName(rawName)
-}
-
-func prettyName(name string) string {
-	if strings.Contains(name, " ") {
-		return name
-	}
-	cleaned := strings.NewReplacer(".", " ", "_", " ").Replace(name)
-	return strings.Join(strings.Fields(cleaned), " ")
+	return rawName
 }
